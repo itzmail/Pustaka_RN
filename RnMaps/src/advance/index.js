7d79065f6d68278afb5757c230a1a3e1bfbe3fcc
Ismail Nur Alam <ismailnuralam@gmail.com>
2021-10-22 21:47:47 +0700

    buat fungsi onMark, onCarouselChange, renderCarousel, dll

--- RnMaps/src/advance/index.js
+++ RnMaps/src/advance/index.js
@@ -4,6 +4,7 @@ import MapView, {
   Marker,
   Callout,
   PROVIDER_GOOGLE,
+  Polygon,
 } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 import Carousel from 'react-native-snap-carousel';
@@ -15,15 +16,17 @@ export default class ReactMaps extends React.Component {
     latitude: 0,
     longitude: 0,
     coordinates: [
-      {name: 'Tengkleng', latitude: 37.43206039081665, longitude: -122.08609767258167, image: require('../assets/food1.jpg'), color: 'purple'},
+      // Google Office
+      /* {name: 'Tengkleng', latitude: 37.43206039081665, longitude: -122.08609767258167, image: require('../assets/food1.jpg'), color: 'purple'},
       {name: 'Sayur', latitude: 37.42165709646536, longitude: -122.09261175245047, image: require('../assets/food2.jpg'), color: 'green' },
-      {name: 'Roti Bakar', latitude: 37.425075408940685, longitude: -122.0774844288826, image: require('../assets/food3.jpg'), color: 'blue'},
-      // {name: '4', latitude: 37.7734153, longitude: -122.4577787},
-      // {name: '5', latitude: 37.7948605, longitude: -122.4596065},
-      // {name: '6', latitude: 37.8025259, longitude: -122.4351431},
+      {name: 'Roti Bakar', latitude: 37.425075408940685, longitude: -122.0774844288826, image: require('../assets/food3.jpg'), color: 'blue'}, */
+      
+      // Magelang
+      {name: 'Tengkleng', latitude: -7.440204297569286, longitude: 110.22746387869121, image: require('../assets/food1.jpg'), color: 'purple'},
+      {name: 'Sayur', latitude: -7.438254122272519, longitude: 110.2282266318798, image: require('../assets/food2.jpg'), color: 'green' },
+      {name: 'Roti Bakar', latitude: -7.442266500442009, longitude: 110.2318784594536, image: require('../assets/food3.jpg'), color: 'blue'},
     ],
-    activeIndex: 0,
-    tryMarker: null
+    marker: []
   };
 
   showMesage() {
@@ -63,7 +66,7 @@ export default class ReactMaps extends React.Component {
           latitude: position.coords.latitude,
           longitude: position.coords.longitude,
           latitudeDelta: 0.009,
-          longitudeDelta: 0.035,
+          longitudeDelta: 0.009,
         }
 
         this.setState({initialPosition: region})
@@ -84,6 +87,30 @@ export default class ReactMaps extends React.Component {
     )
   }
 
+  onCarouselChange = index => {
+    let location = this.state.coordinates[index]
+    // this._map merupakan property dari MapView, dengan property ref
+    this._map.animateToRegion({
+      latitude: location.latitude,
+      longitude: location.longitude,
+      latitudeDelta: 0.009,
+      longitudeDelta: 0.009,
+    })
+
+    this.state.marker[index].showCallout()
+  }
+
+  onMarkPress = (location, index) => {
+    this._map.animateToRegion({
+      latitude: location.latitude,
+      longitude: location.longitude,
+      latitudeDelta: 0.009,
+      longitudeDelta: 0.009,
+    })
+
+    this._carousel.snapToItem(index)
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -99,6 +126,7 @@ export default class ReactMaps extends React.Component {
           coordinate={{latitude:this.state.latitude, longitude: this.state.longitude}}
           title={'San Francisco'}
           onDragEnd={e=> this.setState({tryMarker: e.nativeEvent.coordinate})}
+          
           >
           
           <Callout onPress={() => this.showMesage()}>
@@ -109,12 +137,15 @@ export default class ReactMaps extends React.Component {
             />
           </Callout>
         </Marker>
-        {this.state.coordinates.map(item => {
+        <Polygon coordinates={this.state.coordinates} fillColor={'rgba(100, 200, 300, 0.5)'}/>
+        {this.state.coordinates.map((item, index) => {
           return(
             <Marker key={item.name}
               coordinate={{latitude: item.latitude, longitude: item.longitude}}
               title={item.name}
               pinColor={item.color}
+              ref={ref => this.state.marker[index] = ref}
+              onPress={() => this.onMarkPress(item, index)}
             ></Marker>
           )
         })}
@@ -127,8 +158,9 @@ export default class ReactMaps extends React.Component {
         sliderWidth={Dimensions.get('window').width}
         itemWidth={300}
         itemHeight={300}
-        onSnapToItem = {index => this.setState({activeIndex: index})}
+        onSnapToItem = {index => this.onCarouselChange(index)}
         containerCustomStyle={styles.carousel}
+        removeClippedSubviews={false}
       />
       
       </View>
